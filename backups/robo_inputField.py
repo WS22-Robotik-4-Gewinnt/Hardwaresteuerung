c8cbaf98c0ce68dae7e4240c0c69e747bc6ae589
Niclas Jarowsky <niclas.jarowsky@fh-erfurt.de>
2023-01-24 08:57:14 +0100

    - update README.md - finished main.py for robo logic - added while True for button press send difficulty

--- backups/robo_inputField.py
+++ backups/robo_inputField.py
@@ -1,14 +1,17 @@
-from gpiozero import AngularServo, Button
+from gpiozero import AngularServo, Button, Device
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 import random
 
-lookupTable = [[[-25, 55, 5], [-42, 85, -5], [-53, 85, 22], [-61, 86, 42], [-66, 87, 57], [-68, 87, 71]],
-               [[-35, 60, 5], [-52, 85, 4], [-64, 85, 32], [-72, 86, 49], [-77, 87, 63], [-83, 88, 77]],
-               [[-45, 65, 7], [-61, 85, 9], [-72, 85, 34], [-82, 86, 52], [-83, 79, 73], [-86, 75, 90]],
-               [[38, -24, -44], [51, -36, -53], [64, -48, -63], [77, -60, -68], [90, -72, -73], [90, -68, -90]],
-               [[45, -65, -3], [62, -85, -7], [74, -85, -33], [82, -86, -49], [85, -80, -68], [86, -75, -84]],
-               [[35, -60, 2], [52, -85, -1], [64, -85, -30], [72, -86, -45], [77, -87, -58], [83, -88, -71]],
-               [[25, -55, -2], [42, -85, 5], [54, -85, -22], [61, -86, -39], [66, -87, -52], [68, -87, -65]]]
+Device.pin_factory = PiGPIOFactory()
+
+lookupTable = [[[-20, 55, 2], [-37, 80, -2], [-50, 85, 23], [-59, 86, 43], [-64, 87,60], [-68, 87, 75]],
+               [[-32, 58, 10], [-48, 80, 12], [-61, 85, 32], [-69, 86, 50], [-77, 87, 66], [-83, 88, 80]],
+               [[-40, 62, 10], [-57, 80, 17], [-70, 85, 36], [-80, 86, 54], [-83, 78, 78], [-90, 80, 90]],
+               [[42, -26, -40], [53, -36, -52], [64, -48, -60], [77, -60, -65], [90, -72, -72], [90, -65, -90]],
+               [[48, -65, -5], [64, -80, -14], [75, -86, -30], [82, -86, -44], [85, -80, -64], [86, -76, -81]],
+               [[41, -65, 0], [55, -80, -10], [66, -85, -26], [73, -86, -41], [77, -85, -57], [83, -89, -68]],
+               [[30, -58, 0], [47, -80, -4], [56, -85, -20], [63, -86, -37], [66, -84, -53], [68, -85, -65]]]
 
 stift = AngularServo(13, min_angle=-90, max_angle=90, min_pulse_width=0.0006, max_pulse_width= 0.0024, initial_angle=-90) 	#4
 finger = AngularServo(6, min_angle=-90, max_angle=90, min_pulse_width=0.0006, max_pulse_width= 0.0024, initial_angle=90) 	#3
@@ -58,9 +61,9 @@ def drawLine(fromX, fromY, toX, toY):
   destPos = getPositionAngles(toX, toY)
 
   for x in range(toX - fromX):
-      for y in range(toY - fromY):
-          goto(fromX + x, fromY + y)
-	  sleep(0.5)
+    for y in range(toY - fromY):
+      goto(fromX + x, fromY + y)
+      sleep(0.5)
 
 # Offset für den Stift. Die Platte ist uneben zu der Halterung der Servo Motoren. Durch die vorher angegebene Konstante im Projekt drückt der Stift sonst immer zu stark auf.
 # Deshalb berechnet die Funktion den genauen Winkel, bzw. Druck der auf die Stiftspitze wirkt, damit nicht nach 100 mal zeichnen die Spitze so stark eingedrückt ist, dass sie nur noch auf einer Seite zeichen kann.
@@ -79,40 +82,40 @@ def gotoRandom():
 
 
 while True:
-	# Initial Anfahren auf Startposition
-	reset()
-	sleep(1.5)
+  # Initial Anfahren auf Startposition
+  reset()
+  sleep(1.5)
 
-	#input dialog: catch index out of bounds
-	inputX = -1
-	inputY = -1
-	while(inputX < 0 or inputY < 0 or inputX > 6 or inputY > 5):
+  #input dialog: catch index out of bounds
+  inputX = -1
+  inputY = -1
+  while(inputX < 0 or inputY < 0 or inputX > 6 or inputY > 5):
 
-		print("x: ")
-		inputX = int(input())
-		print("y: ")
-		inputY = int(input())
+    print("x: ")
+    inputX = int(input())
+    print("y: ")
+    inputY = int(input())
 
-		if(inputX < 0 or inputY < 0):
-			print("Fieldindex cannot be negative. Repeat Input")
-        	if(inputX > 6 or inputY > 5):
-			print("Fieldindex out of bounds. Repeat Input")
+    if(inputX < 0 or inputY < 0):
+	    print("Fieldindex cannot be negative. Repeat Input")
+    if(inputX > 6 or inputY > 5):
+	    print("Fieldindex out of bounds. Repeat Input")
 
-	print("going to [" + str(inputX) + "," + str(inputY) + "]")
+  print("going to [" + str(inputX) + "," + str(inputY) + "]")
 
-	#button freigabe
-	print("Press button when ready")
-	btn.wait_for_press()
+  #button freigabe
+  print("Press button when ready")
+  btn.wait_for_press()
 
-	#move
-	goto(inputX, inputY)
-	sleep(1.5)
+  #move
+  goto(inputX, inputY)
+  sleep(1.5)
 
-	#draw
-	down(getOffset(inputY))
-	sleep(0.25)
-	up()
-	sleep(0.5)
+  #draw
+  down(getOffset(inputY))
+  sleep(0.25)
+  up()
+  sleep(0.5)
 
 # Sollte nie erreicht werden, dennoch nach Absturz ggf. Ausgangsposition probieren wiederherzustellen.
 reset()
