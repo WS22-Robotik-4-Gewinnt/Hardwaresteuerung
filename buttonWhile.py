8d9e30ea0ec59dff9dc3d80f23ad0980b7cea748
Niclas Jarowsky <niclas.jarowsky@fh-erfurt.de>
2023-01-24 11:17:12 +0100

    - changed difficulty to original string

--- buttonWhile.py
+++ buttonWhile.py
@@ -6,7 +6,7 @@ readyButton = Button(14)
 
 while True:
   readyButton.wait_for_press()
-  difficulty = '{"row": 3, "col": 3}'
+  difficulty = '{"difficulty": 4}'
   difficulty = json.loads(difficulty)
   result = requests.post(f"http://localhost:8090/ready", json=difficulty)
   print("JSON SEND")
